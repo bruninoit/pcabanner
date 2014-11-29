d846acc47af61ef3107deaa60366b7f324d84083
bruninoit <brunino98@hotmail.it>
2014-11-29 11:42:58 +0100

    creato

---
+++ event/listener.php
@@ -0,0 +1,53 @@
+<?php
+namespace bruninoit\bannerpca\event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+class listener implements
+EventSubscriberInterface{
+protected $config;
+//protected $db;
+//protected $table_prefix;
+//protected $request;
+//protected $search_engines;
+protected $template;
+//protected $auth;
+//protected $user;
+//protected $root_path;
+//protected $phpEx;
+
+
+public function __construct(\phpbb\config\config $config, \phpbb\template\template $template)
+{
+$this->config = $config;
+//$this->db = $db;
+$this->template = $template;
+//$this->auth = $auth;
+//$this->user = $user;
+//$this->root_path = $root_path;
+//$this->phpEx = $phpEx ;
+}
+
+static public function getSubscribedEvents()
+{
+return array(
+'core.user_setup' => 'setup',
+); //cambio evento
+}
+public function setup($event) {
+//file di lingua
+//$lang_set_ext = $event['lang_set_ext'];
+//$lang_set_ext[] = array(
+'ext_name' => 'staffit/toptentopics',
+'lang_set' => 'common',
+);
+//$event['lang_set_ext'] = $lang_set_ext;
+//lingua end
+
+//codice start
+$header=$this->config['banner_header'];
+$footer=$this->config['banner_footer'];
+$this->template->assign_var('BANNER_HEADER', $config_position);
+$this->template->assign_var('BANNER_FOOTER', $config_guest);
+//banner tra primo e secondo post
+//codice end
+}
+}
